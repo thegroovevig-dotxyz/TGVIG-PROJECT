f3b4ee42b66368df73090548caf00966ff64afae
OMO <info@thegroovevig.co.za>
2026-05-03 15:31:53 -0700

    fix vite

--- TGVIG_Backend/server.js
+++ TGVIG_Backend/server.js
@@ -5,6 +5,8 @@ const app = require("./app");
 mongoose.connect("mongodb://127.0.0.1:27017/tgvig");
 
 
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
