3cc61618c852f8436c125f5c49273f6ad5afe6ef
OMO <info@thegroovevig.co.za>
2026-05-03 13:01:37 -0700

    fix WebFront runtime crash (remove invalid state refs)

--- TGVIG_Backend/routes/webContentRoutes.js
+++ TGVIG_Backend/routes/webContentRoutes.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 const ctrl = require("../controllers/webContentController");
 
+// GET section
 router.get("/:section", ctrl.getSection);
+
+// SAVE section
 router.post("/:section", ctrl.saveSection);
+
+// DELETE section
 router.delete("/:section", ctrl.deleteSection);
 
 module.exports = router;
