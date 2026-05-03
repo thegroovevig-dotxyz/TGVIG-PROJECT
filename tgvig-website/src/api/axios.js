f76b16d9515662e5140f71df8bdaa94a640f2f79
OMO <info@thegroovevig.co.za>
2026-05-03 13:09:55 -0700

    fix API baseURL

--- tgvig-website/src/api/axios.js
+++ tgvig-website/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://www.thegroovevig.xyz/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://tgvig-project.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
