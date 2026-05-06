ea7b94d3410a1bc742f7da7f85aa4c0acfd24121
OMO <info@thegroovevig.co.za>
2026-05-06 03:35:53 -0700

    fix frontend routes + mapping errors

--- tgvig-website/src/pages/WebRewards.jsx
+++ tgvig-website/src/pages/WebRewards.jsx
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import API from "../api/axios";
 
 function WebRewards() {
   const [rewards, setRewards] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     API.get("/webcontent/webRewards")
       .then((res) => {
         const data = res.data?.content;
-        if (Array.isArray(data)) {
+         if (Array.isArray(data)) {
         setRewards(data);
       } else if (Array.isArray(data?.items)) {
         setRewards(data.items);
@@ -21,14 +19,14 @@ function WebRewards() {
     .catch(() => setRewards([]));
 }, []);
 
-console.log("REWARDS API RESPONSE:", res.data);
-
   return (
     <div style={{ padding: "20px" }}>
       <h1>Rewards</h1>
 
       {rewards.map((r, i) => (
-        <div key={i} style={{ marginBottom: "15px" }}>
+        <div key={i}
+          onClick={() => navigate("/webrewards")}
+          style={{ cursor: "pointer", marginBottom: "15px" }}>
           {r.image && <img src={r.image} width="200" />}
           <h3>{r.title}</h3>
           <p>{r.description}</p>
