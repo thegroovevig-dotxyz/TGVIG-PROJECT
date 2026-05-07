80999cc235dd975f73e0f5505775f26ec0666eba
OMO <info@thegroovevig.co.za>
2026-05-07 14:46:57 -0700

    Remove old auth context completely

--- tgvig-website/src/auth/Login.jsx
+++ tgvig-website/src/auth/Login.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import useSettings from "../hooks/useSettings";
 
 function Login() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   
