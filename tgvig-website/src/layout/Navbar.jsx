ac46b56f3b0ddfbd816d3e95296e1d81d9516d69
OMO <info@thegroovevig.co.za>
2026-05-09 15:28:27 -0700

    fix Navbar

--- tgvig-website/src/layout/Navbar.jsx
+++ tgvig-website/src/layout/Navbar.jsx
@@ -4,7 +4,7 @@ function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
 
   if (!token) {
-    return <Navigate to="/home" />;
+    return <Navigate to="/home/*" />;
   }
 
   return children;
