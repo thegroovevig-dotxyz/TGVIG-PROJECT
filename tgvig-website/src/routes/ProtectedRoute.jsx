992a2cf9de15e7aa355f3996670d91bdfc764ec1
OMO <info@thegroovevig.co.za>
2026-05-09 14:54:26 -0700

    protectedRoute and App

--- tgvig-website/src/routes/ProtectedRoute.jsx
+++ tgvig-website/src/routes/ProtectedRoute.jsx
@@ -33,7 +33,7 @@ function App() {
             </ProtectedRoute>
           }
         >
-          <Route path="/home" element={<Home />} />
+          <Route path="/home/*" element={<Home />} />
         </Route>
 
       </Routes>
