fa936bde49c77fe1f94b94097e84a0b54109124d
OMO <info@thegroovevig.co.za>
2026-05-10 03:14:19 -0700

    Menu

--- tgvig-website/src/pages/Menu.jsx
+++ tgvig-website/src/pages/Menu.jsx
@@ -4,6 +4,9 @@ import API from "../api/axios";
 function Menu() {
   const [menu, setMenu] = useState([]);
 
+  // store selected size per item
+  const [selectedSize, setSelectedSize] = useState({});
+
   useEffect(() => {
     loadMenu();
   }, []);
@@ -11,11 +14,28 @@ function Menu() {
   const loadMenu = async () => {
     try {
       const res = await API.get("/menu?type=WEB");
-      console.log("MENU:", res.data);
       setMenu(res.data);
     } catch (err) {
       console.log(err);
     }
+  };
+
+  // ✅ FIXED FUNCTION
+  const addToCart = (item) => {
+    const size = selectedSize[item._id];
+
+    if (!size) {
+      alert("Please select Single, x4 or x6");
+      return;
+    }
+
+    console.log("ADD TO CART:", {
+      item,
+      size,
+      price: item.price?.[size],
+    });
+
+    // later: send to cart context or backend
   };
 
   return (
@@ -33,32 +53,68 @@ function Menu() {
             }}
           >
             {item.image && (
-  <img
-    src={item.image.startsWith("http")
-      ? item.image
-      : `http://localhost:5000${item.image}`}
-    alt=""
-    width="100%"
-    style={{ borderRadius: "6px" }}
-  />
-)}
+              <img
+                src={
+                  item.image.startsWith("http")
+                    ? item.image
+                    : `http://localhost:5000${item.image}`
+                }
+                alt=""
+                width="100%"
+                style={{ borderRadius: "6px" }}
+              />
+            )}
 
             <h3>{item.name}</h3>
-            <p><b>{item.type}</b></p>
 
-            {/* PRICES */}
-            <p>Single: R{item.price?.single}</p>
-            <p>x4: R{item.price?.x4}</p>
-            <p>x6: R{item.price?.x6}</p>
+            {/* SIZE SELECTOR */}
+            <div style={{ marginBottom: "10px" }}>
+              <label>
+                <input
+                  type="radio"
+                  name={item._id}
+                  onChange={() =>
+                    setSelectedSize((prev) => ({
+                      ...prev,
+                      [item._id]: "single",
+                    }))
+                  }
+                />
+                Single (R{item.price?.single})
+              </label>
 
-            {/* SPECIAL */}
-            {item.type === "SPECIAL" && (
-              <>
-                <p> Points: {item.rewards?.points}</p>
-                <p> Reward: {item.rewards?.itemReward}</p>
-                <p> Discount: {item.rewards?.discount}%</p>
-              </>
-            )}
+              <br />
+
+              <label>
+                <input
+                  type="radio"
+                  name={item._id}
+                  onChange={() =>
+                    setSelectedSize((prev) => ({
+                      ...prev,
+                      [item._id]: "x4",
+                    }))
+                  }
+                />
+                x4 (R{item.price?.x4})
+              </label>
+
+              <br />
+
+              <label>
+                <input
+                  type="radio"
+                  name={item._id}
+                  onChange={() =>
+                    setSelectedSize((prev) => ({
+                      ...prev,
+                      [item._id]: "x6",
+                    }))
+                  }
+                />
+                x6 (R{item.price?.x6})
+              </label>
+            </div>
 
             <button onClick={() => addToCart(item)}>
               Add to Cart
