89941dd95c49355bb06176537cb4d03c02078774
OMO <info@thegroovevig.co.za>
2026-05-11 03:29:34 -0700

    Profile

--- tgvig-website/src/pages/Profile.jsx
+++ tgvig-website/src/pages/Profile.jsx
@@ -8,19 +8,14 @@ function Profile() {
   useEffect(() => {
   const user = authService.getUser();
 
-  if (!user || !user._id) {
-    console.log("No user or missing _id");
+  if (!user || !user.id) {
+    console.log("No user or missing id");
     return;
   }
 
-  getMember(user._id)
-    .then((res) => {
-      console.log("PROFILE RESPONSE:", res.data);
-      setProfile(res.data);
-    })
-    .catch((err) => {
-      console.log("PROFILE ERROR:", err.response?.data || err.message);
-    });
+  getMember(user.id)
+    .then((res) => setProfile(res.data.member))
+    .catch(console.log);
 }, []);
 
 
@@ -28,16 +23,17 @@ function Profile() {
   <div style={{ padding: "20px" }}>
     <h2>PROFILE</h2>
 
-    <p><b>Name:</b> {profile?.firstName} {profile?.lastName}</p>
-    <p><b>Email:</b> {profile?.email}</p>
-    <p><b>Membership No:</b> {profile?.membershipNo}</p>
-    <p><b>Role:</b> {profile?.role}</p>
-    <p><b>Status:</b> {profile?.status}</p>
-    <p><b>Tier:</b> {profile?.tier}</p>
-    <p><b>Wallet:</b> R {profile?.walletBalance}</p>
-    <p><b>Points:</b> {profile?.pointsBalance}</p>
+    <p><b>Name:</b> {profile.firstName} {profile.lastName}</p>
+        <p><b>Email:</b> {profile.email}</p>
+        <p><b>Membership No:</b> {profile.membershipNo}</p>
+        <p><b>Role:</b> {profile.role}</p>
+        <p><b>Status:</b> {profile.status}</p>
+        <p><b>Tier:</b> {profile.tier}</p>
+        <p><b>Wallet:</b> R {profile.walletBalance}</p>
+        <p><b>Points:</b> {profile.pointsBalance}</p>
   </div>
 );
 }
 
 export default Profile;
+
