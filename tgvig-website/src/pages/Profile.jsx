eda6a87faa858ae7a45a41dde0eabd9f861c2988
OMO <info@thegroovevig.co.za>
2026-05-11 01:38:57 -0700

    Profile

--- tgvig-website/src/pages/Profile.jsx
+++ tgvig-website/src/pages/Profile.jsx
@@ -14,26 +14,28 @@ function Profile() {
   }
 
   getMember(user.id)
-    .then((res) => setProfile(res.data))
+    .then((res) => setProfile(res.data.member))
     .catch(console.log);
 }, []);
+
+
 
   if (!profile) return <p>Loading...</p>;
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>PROFILE</h2>
+  <div style={{ padding: "20px" }}>
+    <h2>PROFILE</h2>
 
-       <p><b>Name:</b> {profile.firstName} {profile.lastName}</p>
-        <p><b>Email:</b> {profile.email}</p>
-        <p><b>Membership No:</b> {profile.membershipNo}</p>
-        <p><b>Role:</b> {profile.role}</p>
-        <p><b>Status:</b> {profile.status}</p>
-        <p><b>Tier:</b> {profile.tier}</p>
-        <p><b>Wallet:</b> R {profile.walletBalance}</p>
-        <p><b>Points:</b> {profile.pointsBalance}</p>
-    </div>
-  );
+    <p><b>Name:</b> {profile?.firstName} {profile?.lastName}</p>
+    <p><b>Email:</b> {profile?.email}</p>
+    <p><b>Membership No:</b> {profile?.membershipNo}</p>
+    <p><b>Role:</b> {profile?.role}</p>
+    <p><b>Status:</b> {profile?.status}</p>
+    <p><b>Tier:</b> {profile?.tier}</p>
+    <p><b>Wallet:</b> R {profile?.walletBalance}</p>
+    <p><b>Points:</b> {profile?.pointsBalance}</p>
+  </div>
+);
 }
 
 export default Profile;
