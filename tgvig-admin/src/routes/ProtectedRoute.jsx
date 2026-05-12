49574a02c5c6d080344be7bd299a03c72f595aa2
OMO <info@thegroovevig.co.za>
2026-05-12 06:03:39 -0700

    fix ProtectedRoute

--- tgvig-admin/src/routes/ProtectedRoute.jsx
+++ tgvig-admin/src/routes/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
   const user = JSON.parse(localStorage.getItem("user"));
 
-  if (!token || user?.role !== "admin") {
+  if (!token || user?.role !== "ADMIN") {
     return <Navigate to="/" />;
   }
 
