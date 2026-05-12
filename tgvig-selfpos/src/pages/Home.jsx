a1dd8a130704ba19fe22a039d4f700a87fbfad4e
OMO <info@thegroovevig.co.za>
2026-05-12 13:04:20 -0700

    fix Home

--- tgvig-selfpos/src/pages/Home.jsx
+++ tgvig-selfpos/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const deviceId = import.meta.env.VITE_DEVICE_ID;
       <h1>WELCOME TO SELF POS</h1>
 
       <button
-        onClick={() => navigate('/MenuGrid')}
+        onClick={() => navigate(`/selfpos/${clubId}/${deviceId}`)}
       >
         Start Order
       </button>
