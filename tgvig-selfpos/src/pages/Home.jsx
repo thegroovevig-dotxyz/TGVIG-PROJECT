83399a0703bdb9cbd707aa9a159436ac1cf78830
OMO <info@thegroovevig.co.za>
2026-05-12 07:13:53 -0700

    fix Home

--- tgvig-selfpos/src/pages/Home.jsx
+++ tgvig-selfpos/src/pages/Home.jsx
@@ -1,18 +1,30 @@
 import API from "../api/axios";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { getBlogs } from "../api/blogs.api";
 
 
 
 function Home() {
+   const [blogs, setBlogs] = useState([]);
   const navigate = useNavigate();
 
   const clubId = "69e9284d26961ca9fbef1c08";
   const deviceId = "69f32f056119ac5b6afe3b9b"; // your SELF POS device
 
+
   useEffect(() => {
     API.get("/blogs").then(res => setBlogs(res.data));
   }, []);
+
+  const loadBlogs = async () => {
+    try {
+      const res = await getBlogs();
+      setBlogs(res.data || []);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
