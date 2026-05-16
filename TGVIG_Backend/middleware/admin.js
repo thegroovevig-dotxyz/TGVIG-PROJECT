812f4673403ee77d6c6b63deb7fddb175866c7bd
OMO <info@thegroovevig.co.za>
2026-05-16 10:32:55 -0700

    fix middleware

---
+++ TGVIG_Backend/middleware/admin.js
@@ -0,0 +1,10 @@
+module.exports = (req, res, next) => {
+
+  if (!req.user || req.user.role !== "ADMIN") {
+    return res.status(403).json({
+      message: "Admin access required"
+    });
+  }
+
+  next();
+};
