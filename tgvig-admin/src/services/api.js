b0501b776997fc0ae1103a8039ebc424c1bbbf1b
OMO <info@thegroovevig.co.za>
2026-05-17 02:31:42 -0700

    Fix API service path structure

--- tgvig-admin/src/services/api.js
+++ tgvig-admin/src/services/api.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5000/API"
-      : "https://tgvig-project-backend.onrender.com/API",
+  baseURL: "/API",
+});
+
+API.interceptors.request.use((req) => {
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
 });
 
 export default API;
