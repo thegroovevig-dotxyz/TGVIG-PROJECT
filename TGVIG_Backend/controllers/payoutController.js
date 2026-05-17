3846e00b7cb09f8fb817bf50eaace23a3c1e2bb8
OMO <info@thegroovevig.co.za>
2026-05-17 03:07:39 -0700

    Fix PAYOUT CONTROLLER

--- TGVIG_Backend/controllers/payoutController.js
+++ TGVIG_Backend/controllers/payoutController.js
@@ -1,6 +1,4 @@
 const Transaction = require("../models/Transaction");
-const Driver = require("../models/Driver");
-const Partner = require("../models/Partner");
 
 // 💸 GET PENDING PAYOUTS
 exports.getPendingPayouts = async (req, res) => {
@@ -18,14 +16,71 @@ exports.getPendingPayouts = async (req, res) => {
     });
 
   } catch (err) {
+
+    console.log(err);
+
     res.status(500).json({
       message: err.message
     });
+
   }
 };
 
 
-// 💸 MARK PAYOUT AS PAID
+// 📊 GET ALL PAYOUTS
+exports.getAllPayouts = async (req, res) => {
+  try {
+
+    const payouts = await Transaction.find();
+
+    res.json({
+      success: true,
+      payouts
+    });
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      message: err.message
+    });
+
+  }
+};
+
+
+// 📊 GET MY PAYOUTS
+exports.getMyPayouts = async (req, res) => {
+  try {
+
+    const userId = req.user._id;
+
+    const payouts = await Transaction.find({
+      $or: [
+        { driverId: userId },
+        { partnerId: userId }
+      ]
+    });
+
+    res.json({
+      success: true,
+      payouts
+    });
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      message: err.message
+    });
+
+  }
+};
+
+
+// 💸 MARK PAID
 exports.markPaid = async (req, res) => {
   try {
 
@@ -45,81 +100,16 @@ exports.markPaid = async (req, res) => {
 
     res.json({
       success: true,
-      message: "Payout marked as paid"
-    });
-
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
-    });
-  }
-};
-
-// 📊 GET ALL PAYOUTS (ADMIN)
-exports.getAllPayouts = async (req, res) => {
-  try {
-
-    const payouts =
-      await payoutService.calculatePayouts();
-
-    res.json({
-      success: true,
-      payouts
-    });
-
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
-    });
-  }
-};
-
-
-// 📊 GET MY PAYOUTS (DRIVER / PARTNER)
-exports.getMyPayouts = async (req, res) => {
-  try {
-
-    const userId = req.user._id;
-    const role = req.user.role;
-
-    const result =
-      await payoutService.getPayoutSummary({
-        userId,
-        role
-      });
-
-    res.json({
-      success: true,
-      data: result
-    });
-
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
-    });
-  }
-};
-
-
-// 💸 MARK PAID (ADMIN ONLY)
-exports.markPaid = async (req, res) => {
-  try {
-
-    const { transactionId } = req.body;
-
-    const tx =
-      await payoutService.markAsPaid({
-        transactionId
-      });
-
-    res.json({
-      success: true,
       tx
     });
 
   } catch (err) {
+
+    console.log(err);
+
     res.status(500).json({
       message: err.message
     });
+
   }
 };
